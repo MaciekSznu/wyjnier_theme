6d6d7d2955e3e6026b98b55276504f27ec25c6a7
Maciej Sznurawa <msznurawa@gmail.com>
2020-08-09 14:51:04 +0200

    Add search section

---
+++ archive-oferty.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<?php get_template_part('includes/hero', 'oferty'); ?>
+
+<?php get_template_part('includes/search_offer'); ?>
+<?php //get_template_part('includes/content', 'oferty'); ?>
+
+
+<?php get_footer(); ?>
---
+++ includes/search_offer.php
@@ -0,0 +1,31 @@
+<section class='search'>
+  <div class='search-wrapper'>
+    <form class='search-form' action="">
+      <div class='search-form-selects-wrapper'>
+        <div class='single-select-wrapper'>
+          <select class='search-form__property' name="property" id="property">
+            <option class='search-form__property--item' value='wszystkie'>Wszystkie oferty</option>
+            <option class='search-form__property--item' value='domy'>Domy</option>
+            <option class='search-form__property--item' value='mieszkania'>Mieszkania</option>
+            <option class='search-form__property--item' value='dzialki'>Działki</option>
+            <option class='search-form__property--item' value='lokale'>Lokale</option>
+            <option class='search-form__property--item' value='apartamenty'>Apartamenty wakacyjne</option>
+          </select>
+          <div class='select-arrow'></div>
+        </div>
+        <div class='single-select-wrapper'>
+          <select class='search-form__transaction' name="transaction" id="transaction">
+            <option class='search-form__transaction--item' value='wszystkie'>Wszystkie transakcje</option>
+            <option class='search-form__transaction--item' value='sprzedaż'>Sprzedaż</option>
+            <option class='search-form__transaction--item' value='wynajem'>Wynajem</option>
+          </select>
+        <div class='select-arrow'></div>
+        </div>
+      </div>
+      <button class='search-form__button' type='submit'>
+        <img class='search-form__button--icon'src="../wp-content/themes/wyjnier/images/icons/search.svg" alt="Search">
+        szukaj
+      </button>
+    </form>
+  </div>
+</section>
